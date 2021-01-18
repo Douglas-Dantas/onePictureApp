d7fe4dbb0da7c8f97c7a05fa468dace63750f009
Douglas Dantas <douglasdantas@mobits.com>
2021-01-18 20:44:47 -0300

    Adicionado animação no botão adicionar foto e animação de zoom no gridView

--- lib/Telas/TelaInicial.dart
+++ lib/Telas/TelaInicial.dart
@@ -32,15 +32,19 @@ class _TelaInicialState extends State<TelaInicial> {
   var _scaleStart;
   var _scaleEnd;
   var _corBotoesOpcoes;
+  int _duracaoAnimacao;
+  int _numberOfTilesPerRow;
 
   bool shouldShowOptions;
   bool shouldHideOptions;
 
   @override
   void initState() {
-    _corBotoesOpcoes = Colors.yellowAccent;
+    _corBotoesOpcoes = Colors.teal;
+    _duracaoAnimacao = 150;
     shouldShowOptions = false;
     shouldHideOptions = true;
+    _numberOfTilesPerRow = 3;
     super.initState();
   }
 
@@ -52,16 +56,21 @@ class _TelaInicialState extends State<TelaInicial> {
       ),
       body: GestureDetector(
         onScaleUpdate: (details) {
+          if (details.scale > 1)
+            _numberOfTilesPerRow = 4;
+          else
+            _numberOfTilesPerRow = 3;
           setState(() {
-            var scale = details.scale;
-            _scale = "$scale";
+            _scale = "${details.scale}";
           });
         },
-        child: GridView.count(
-          crossAxisCount: 3,
-          children: lista,
-          physics: BouncingScrollPhysics(),
-          padding: EdgeInsets.all(2),
+        child: AnimatedCrossFade(
+          duration: Duration(milliseconds: 500),
+          firstChild: gridView(3),
+          secondChild: gridView(4),
+          crossFadeState: _numberOfTilesPerRow == 3
+              ? CrossFadeState.showFirst
+              : CrossFadeState.showSecond,
         ),
       ),
       floatingActionButton: Container(
@@ -72,39 +81,60 @@ class _TelaInicialState extends State<TelaInicial> {
     );
   }
 
+  Widget gridView(numberOfTilesPerRow) {
+    return GridView.count(
+      shrinkWrap: true,
+      crossAxisCount: numberOfTilesPerRow,
+      children: lista,
+      physics: BouncingScrollPhysics(),
+      padding: EdgeInsets.all(2),
+    );
+  }
+
   Widget floatingActionButton() {
     return Stack(
       alignment: Alignment.bottomCenter,
+      overflow: Overflow.visible,
       children: [
         AnimatedPositioned(
-          duration: Duration(milliseconds: 200),
-          bottom: shouldShowOptions ? 60 : -100,
-          // onEnd: () => setState(() {
-          // if (!shouldShowOptions) shouldHideOptions = !shouldHideOptions;
-          // },),
+          duration: Duration(milliseconds: _duracaoAnimacao),
+          bottom: shouldShowOptions ? 60 : 0,
           child: Column(
             children: [
-              IconButton(
-                  icon: Icon(
+              RawMaterialButton(
+                  child: Icon(
                     Icons.camera_alt,
+                    color: Colors.white,
                   ),
-                  color: _corBotoesOpcoes,
+                  fillColor: _corBotoesOpcoes,
+                  shape: CircleBorder(),
                   onPressed: () => clicouCamera()),
-              IconButton(
-                  icon: Icon(
-                    Icons.photo_album,
-                  ),
-                  color: _corBotoesOpcoes,
-                  onPressed: () => print('Clicou Album')),
             ],
+          ),
+        ),
+        AnimatedPositioned(
+          duration: Duration(milliseconds: _duracaoAnimacao),
+          bottom: shouldShowOptions ? 105 : 0,
+          child: Tooltip(
+            message: "Abrir Álbum",
+            child: RawMaterialButton(
+                child: Icon(
+                  Icons.photo_album,
+                  color: Colors.white,
+                ),
+                fillColor: _corBotoesOpcoes,
+                shape: CircleBorder(),
+                onPressed: () => print('Clicou Album')),
           ),
         ),
         FloatingActionButton(
           onPressed: () => clicouFloatingActionButton(),
-          tooltip: 'Abrir Câmera',
-          elevation: shouldShowOptions ? 10 : 2,
+          tooltip: 'Adicionar Foto',
+          elevation: 10,
+          backgroundColor: shouldShowOptions ? Colors.red : _corBotoesOpcoes,
           child: Icon(
             shouldShowOptions ? Icons.cancel : Icons.add_a_photo,
+            // color: ,
             // size: 45,
           ),
         ),
